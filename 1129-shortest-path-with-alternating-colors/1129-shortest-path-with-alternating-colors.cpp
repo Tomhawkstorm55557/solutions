cbf22fa2241d06c1564fa041eda7aabbc1acb131
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-02-11 23:38:53 +0530

    Time: 23 ms (65.09%), Space: 15.9 MB (28.52%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -4,24 +4,32 @@ class Solution {
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>> &redEdges, vector<vector<int>> &blueEdges){
 
         vector<int> dis(n , -1);
+        //making ajacent list in a way to store 
         vector<pair<int, int>> adj[n + 1];
+        // storing the all edges with color tag 1
         for (auto vec : redEdges){
             adj[vec[0]].push_back({vec[1], 1});
         }
+        // storing all the edges with color 2
         for (auto vec : blueEdges){
             adj[vec[0]].push_back({vec[1], 2});
         }
+        // building queue
         queue<vector<int>> q;
+        // building queue that store three value edge dis and color tag
         q.push({0, 0, -1});
+        // running until the queue 
         while (q.size() > 0){
             auto currVec = q.front();
             q.pop();
             int currv = currVec[0], edgeColor = currVec[2];
+            // agar uksa distance vector ma default value haa toh usaa update kardoo
             if (dis[currv] == -1){
                 dis[currv] = currVec[1];
             }
             for (auto &childPair : adj[currv]){
                 int childV = childPair.first, edgeColorOfChild = childPair.second;
+                // yeh dekhenga isma kya yeh visited haa ya nhi ya iska rdge color same haa ya nhi 
                 if (childV != -1 && (edgeColor == -1 || edgeColor != edgeColorOfChild)){
                     q.push({childV, currVec[1] + 1, edgeColorOfChild});
                     childPair.first = -1;
