a9593e35fc92e3b4a9f83e9897f9c2227a908c15
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-02-18 14:39:21 +0530

    Time: 3 ms (70.32%), Space: 6 MB (69.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    int fibo(int n,vector<int>&dp)
-    {
-        if(n==0) return 0;
-        if(n==1) return 1;
-
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-    }
     int fib(int n) {
-     vector<int>dp(n+1,-1);
-     return fibo(n,dp); 
+            if(n==0||n==1){
+            return n;
+        }
+        
+        // 1 d dp problem 
+        vector<int>dp(n+1,-1);
+         
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] +dp[i-2];
+        }
+        return dp[n];
+        
+        
+        
     }
 };
