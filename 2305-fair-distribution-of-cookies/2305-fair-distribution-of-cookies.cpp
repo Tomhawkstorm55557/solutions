51898195fba0cea5a0a0b14068b14c63c33154fc
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-03-13 14:54:00 +0530

    Time: 1623 ms (38.46%), Space: 7 MB (45.45%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int ans=INT_MAX;
-    void backtrack(vector<int>& cookies, int k,vector<int> &v,int idx)
-    {
-        if(idx>=cookies.size())
-        {
-            int res=INT_MIN;
-            for(int i=0;i<k;i++)
-            {
-                res=max(res,v[i]);
-            }
-            ans=min(ans,res);
-            return;
-        }
-        for(int i=0;i<k;i++)
-        {
-            v[i]+=cookies[idx];
-            backtrack(cookies,k,v,idx+1);
-            v[i]-=cookies[idx];
-        }
-    }
-    int distributeCookies(vector<int>& cookies, int k) 
-    {
-        vector<int> v(k,0);
-        backtrack(cookies,k,v,0);
-        return ans;
+    int ans =INT_MAX;
+  void backtrack(vector<int>& cookies,int k,int indx,vector<int>&v){
+        if(indx>=cookies.size()){
+            int res = INT_MIN;
+        for(int i =0;i<k;i++){
         
+            res=max(res,v[i]);
+            
+            
+        }
+            ans = min(ans,res);
+            return ;
     }
-};
+    for(int i =0;i<k;i++){
+        v[i]+=cookies[indx];
+        backtrack(cookies,k,indx+1,v);
+        v[i]-=cookies[indx];
+    }   
+  }    
+int distributeCookies(vector<int>& cookies, int k) {
+    vector<int> v(k,0);
+        backtrack(cookies,k,0,v);
+        return ans;
+    }
+};  
+   
