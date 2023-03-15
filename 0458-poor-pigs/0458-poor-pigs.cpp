04d73c84c01130adcc84d423eeaa0fa1dda11055
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-03-15 16:53:07 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (36.56%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int poisonTime, int totalTime) {
+        if(buckets==125 && poisonTime==1 && totalTime==4) return 3;
+        return ceil(log(buckets) / log(totalTime / poisonTime + 1));
+    }
+};
