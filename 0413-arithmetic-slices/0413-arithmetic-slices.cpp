60accf8a13835441e3fbb3a6fc05f292653f12af
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-03-16 17:44:19 +0530

    Time: 6 ms (20.42%), Space: 7.3 MB (59.38%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -10,6 +10,9 @@ public:
         }
         for(int i=2;i<n;i++){
             if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+             //So while storing 2nd ans in dp, we'll simply add the previous 1 to our current dp.
+//dp[i] = 1 + dp[i-1]
+//Add the current dp count to our answer variable, count += dp[i];   
                 dp[i] = 1 + dp[i-1];
                 count+=dp[i];
             }
