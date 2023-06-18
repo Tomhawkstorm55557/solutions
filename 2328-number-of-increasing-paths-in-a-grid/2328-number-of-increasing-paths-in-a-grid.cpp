928c8c2cb55925915c5955d3b804181c2b6e2570
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-06-19 01:55:58 +0530

    Time: 284 ms (82.77%), Space: 43.5 MB (63.87%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    //Return the number of strictly increasing paths in the grid such that you can start from any cell and end at any cell. Since the answer may be very large, return it modulo 109 + 7.
+    
+    //Two paths are considered different if they do not have exactly the same sequence of visited cells.
+    const int MOD = 1e9 + 7;
+    int solve(int i, int j, int n,int  m,vector<vector<int>>& grid,    vector<vector<int>>&dp){
+         if (i < 0 or j < 0 or i >= n or j >= m)
+            return 0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+         
+        int up = 0, down = 0, left = 0, right = 0;
+        if (i - 1 >= 0 and grid[i - 1][j] > grid[i][j])
+            up = solve(i - 1, j, n, m, grid, dp);
+        
+        if (i + 1 < n and grid[i + 1][j] > grid[i][j])
+            down = solve(i + 1, j, n, m, grid, dp);
+        
+        if (j - 1 >= 0 and grid[i][j - 1] > grid[i][j])
+            left = solve(i, j - 1, n, m, grid, dp);
+        
+        if (j + 1 < m and grid[i][j + 1] > grid[i][j])
+            right = solve(i, j + 1, n, m, grid, dp);
+        
+        return dp[i][j] = (1 + up + down + left + right) % MOD;
+    }
+    
+    int countPaths(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+         vector<vector<int>>dp(n, vector<int>(m, -1));
+        int ans = 0;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                 ans = (ans + solve(i, j, n, m, grid, dp)) % MOD;
+            }
+        }
+        return ans%MOD;
+    }
+};
