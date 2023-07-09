1c4f4ea9989ca78a63af14171efd07f0930e3518
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-07-10 03:25:09 +0530

    Time: 64 ms (23.47%), Space: 41.8 MB (69.49%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,11 @@
+/**
+ * @return {number}
+ */
+///Write a function argumentsLength that returns the count of arguments passed to it.
+var argumentsLength = function(...args) {
+    return arguments.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
