c475784e0aebe6b9905828ac729ca2461fb91ed9
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-08-17 18:47:15 +0530

    Time: 591 ms (35.15%), Space: 0B (100.00%) - LeetHub

---
+++ 0595-big-countries/0595-big-countries.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+#A country is big if:
+
+#it has an area of at least three million (i.e., 3000000 km2), or
+#it has a population of at least twenty-five million (i.e., 25000000).
+#Write a solution to find the name, population, and area of the big countries.
+SELECT name, population, area FROM World WHERE area >= 3000000 || population >= 25000000;
