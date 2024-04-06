afd38ab5582884038a37ba0cccb7b60504ee3d88
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2024-04-06 15:49:06 +0530

    Time: 77 ms (60.37%), Space: 21.2 MB (54.49%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-         if(s1==s2)
+        if(s1==s2){
             return true;
-        
-        unordered_map<char,int> evens1, odds1, evens2, odds2;
+        }
+        unordered_map<char,int>odd1,odd2,even1,even2;
         int n = s1.size();
-		
-		//count the frequency of characters at even and odd places separately for both the strings
-        for(int i=0;i<n;i++)
-        {
-            if(i%2==0)//even place
-            {
-                evens1[s1[i]]++;
-                evens2[s2[i]]++;
-                
+        for(int i =0;i<n;i++){
+            if(i%2==0){
+                even1[s1[i]]++;
+                even2[s2[i]]++;
             }
-            else // odd place
-            {
-                odds1[s1[i]]++;
-                odds2[s2[i]]++;
+            else{
+                odd1[s1[i]]++;
+                odd2[s2[i]]++;
             }
         }
-		
-		//if the frequency map for even and odd places for both the strings is equal simultaneously then strings will be equal after swapping  operation otherwise not
-        return (evens1== evens2 && odds1 ==odds2)?true:false;
+           return (even1== even2 && odd1 ==odd2)?true:false;
     }
 };
