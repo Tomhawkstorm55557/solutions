0f8477aa8cf50c89e2f8294a53298a11aa5464ca
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2024-04-06 18:23:39 +0530

    Time: 157 ms (21.37%), Space: 76.2 MB (12.76%) - LeetHub

--- 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.cpp
+++ 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.cpp
@@ -1,36 +1,36 @@
 class Solution {
+   
 public:
+     #define  ll long long
+    ///Input: nums = [2,6,7,3,1,7], m = 3, k = 4
     long long maxSum(vector<int>& nums, int m, int k) {
-        map<int,int> mp;  
-        long long sum=0;
-        long long ans=-1;
-        for(int i=0;i<k;i++){
+        /// case 1 when n is equal to k 
+        map<int,int>mp;
+        ll ans = -1;
+        ll sum =0;
+        for(int i =0;i<k;i++){
             mp[nums[i]]++;
             sum+=nums[i];
         }
-        if(mp.size()>=m){
+          if(mp.size()>=m){
             ans=max(ans,sum);
         }
-        int x=0;
-        for(int i=k;i<nums.size();i++){
-            
-            sum-=nums[x];
+        int x =0;
+        int n = nums.size();
+        for(int i =k;i<n;i++){
             mp[nums[x]]--;
-            if(mp[nums[x]]==0) mp.erase(nums[x]);
+            sum-=nums[x];
+           if(mp[nums[x]]==0){
+               mp.erase(nums[x]);
+           }
+              mp[nums[i]]++;
             sum+=nums[i];
-            mp[nums[i]]++;
-            
-            if(mp.size()>=m){
-              ans=max(ans,sum);
-                
-            }
-            
-            
+             if(mp.size()>=m){
+            ans=max(ans,sum);
+        }
             x++;
         }
-        
-        if(ans==-1) return 0; 
+           if(ans==-1) return 0; 
         return ans;
-       
     }
 };
